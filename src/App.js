af6af501c4f6c5acf1670092c6dcc854e4ef3ac2
KOiSPRK <sparkerk@gmail.com>
2018-03-28 11:59:32 +0700

    Authorised Routes
    Dashboard page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Homepage from "./components/pages/Homepage";
 import LoginPage from "./components/pages/LoginPage";
+import DashboardPage from "./components/pages/DashboardPage";
+import UserRoute from "./components/routes/UserRoute";
 
 const App = () =>
 <div className="ui container">
   <Route path="/" exact component={Homepage} />
   <Route path="/login" exact component={LoginPage} />
+  <UserRoute path="/dashboard" exact component={DashboardPage} />
 </div>
 
 export default App;
