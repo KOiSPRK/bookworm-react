96136893311d0214ce042f7690f9dddf439b4eed
KOiSPRK <sparkerk@gmail.com>
2018-03-28 11:03:29 +0700

    Keep Logging in state in localStorage

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const Homepage = () => <div>
+const Homepage = ({ isAuthenticated }) => <div>
   <h1>Home Page</h1>
-  <Link to="/login">Login</Link>
+  { isAuthenticated ? <button>Logout</button> : <Link to="/login">Login</Link> }
 </div>
 
-export default Homepage;
+Homepage.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.user.token
+  }
+}
+
+export default connect(mapStateToProps)(Homepage);
