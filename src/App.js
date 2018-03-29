a4ae4dbeb07d935a2c777bdfb4bf40053ed1c428
KOiSPRK <sparkerk@gmail.com>
2018-03-29 12:19:53 +0700

    Log Out and Top Navigation

--- src/App.js
+++ src/App.js
@@ -11,16 +11,18 @@ import UserRoute from "./components/routes/UserRoute";
 import GuestRoute from "./components/routes/GuestRoute";
 import ForgotPasswordPage from "./components/pages/ForgotPasswordPage";
 import ResetPasswordPage from "./components/pages/ResetPasswordPage";
+import TopNavigation from "./components/navigation/TopNavigation";
 
 const App = ({ location, isAuthenticated }) => (
   <div className="ui container">
-  <Route location={location} path="/" exact component={Homepage} />
-  <Route location={location} path="/confirmation/:token" exact component={ConfirmationPage} />
-  <GuestRoute location={location} path="/login" exact component={LoginPage} />
-  <GuestRoute location={location} path="/signup" exact component={SignupPage} />
-  <GuestRoute location={location} path="/forgot_password" exact component={ForgotPasswordPage} />
-  <GuestRoute location={location} path="/reset_password/:token" exact component={ResetPasswordPage} />
-  <UserRoute location={location} path="/dashboard" exact component={DashboardPage} />
+    {isAuthenticated && <TopNavigation />}
+    <Route location={location} path="/" exact component={Homepage} />
+    <Route location={location} path="/confirmation/:token" exact component={ConfirmationPage} />
+    <GuestRoute location={location} path="/login" exact component={LoginPage} />
+    <GuestRoute location={location} path="/signup" exact component={SignupPage} />
+    <GuestRoute location={location} path="/forgot_password" exact component={ForgotPasswordPage} />
+    <GuestRoute location={location} path="/reset_password/:token" exact component={ResetPasswordPage} />
+    <UserRoute location={location} path="/dashboard" exact component={DashboardPage} />
 </div>
 );
 
