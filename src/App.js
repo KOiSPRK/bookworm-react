edc67468deb582a66488162017e69392244cc81e
KOiSPRK <sparkerk@gmail.com>
2018-03-29 09:04:45 +0700

    Send Confirmation Email

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Homepage from "./components/pages/Homepage";
 import LoginPage from "./components/pages/LoginPage";
 import DashboardPage from "./components/pages/DashboardPage";
 import SignupPage from "./components/pages/SignupPage";
+import ConfirmationPage from "./components/pages/ConfirmationPage";
 import UserRoute from "./components/routes/UserRoute";
 import GuestRoute from "./components/routes/GuestRoute";
 
 const App = ({ location, isAuthenticated }) => (
   <div className="ui container">
   <Route location={location} path="/" exact component={Homepage} />
+  <Route location={location} path="/confirmation/:token" exact component={ConfirmationPage} />
   <GuestRoute location={location} path="/login" exact component={LoginPage} />
   <GuestRoute location={location} path="/signup" exact component={SignupPage} />
   <UserRoute location={location} path="/dashboard" exact component={DashboardPage} />
